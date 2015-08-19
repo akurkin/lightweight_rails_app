fbd4cac0f6440c2f881dd4b7ec689103118a92a4
Alex Kurkin <thaold@thaold.com>
2015-08-19 10:20:04 +0200

    test setup.rb a bit more

--- cd/setup.rb
+++ cd/setup.rb
@@ -1,12 +1,18 @@
 branch = ENV['CIRCLE_BRANCH']
 jira_card = if branch =~ /feature\/(.*)/
-  $1.to_s
-else
-  ''
-end
+              $1.to_s
+            else
+              ''
+            end
 
-stack_name = "quotes-#{jira_card}"
-short_commit = `git rev-parse --short=4 $CIRCLE_SHA1`.chomp
+stack_name = if jira_card != ''
+               "quotes-#{jira_card}"
+             else
+               'quotes'
+             end
+
+circle_commit = ENV['CIRCLE_SHA1']
+short_commit = `git rev-parse --short=4 #{circle_commit}`.chomp
 
 puts "export CUSTOM_JIRA_CARD=#{jira_card}"
 puts "export CUSTOM_BRANCH=#{branch}"
